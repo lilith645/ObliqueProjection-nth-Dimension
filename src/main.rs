56d3706606a7603c10fcb15d4c14484663cd3232
Lilith645 <lilith@inet-sys.com>
2017-01-23 15:41:33 +1000

    Cleaned functions, add example cube data, reads from csv Changes committed: 	modified:   Cargo.toml 	new file:   data/cube.csv 	renamed:    src/main.rs~ -> src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -22,10 +22,12 @@
 
 extern crate glium;
 extern crate image;
+extern crate csv;
 
 use std::f64;
 use std::fs::File;
 use std::path::Path;
+use std::fmt::Display;
 
 #[derive(Copy, Clone)] // Some random thing to define the struct to do stuff? i think?
 struct Vertex3f {
@@ -40,45 +42,21 @@ struct Vertex2f {
 implement_vertex!(Vertex3f, position);
 implement_vertex!(Vertex2f, position);
 
-fn convert_point_3d_to_2d(point: Vec<Vertex3f>) -> Vec<Vertex2f> {
-  let angle: f64 = 63.4;
-  
-  let mut new_point = Vec::new();
-  
-  for i in point {
-     new_point.push( Vertex2f { position: { [i.position[0] + 0.5*i.position[2]*angle.cos(), i.position[1] + 0.5*i.position[2]*angle.sin()] } });
-  }
-   
-  new_point
-}
-
-fn main() {    
-    let mut point = Vec::new();
-    
-    point.push(Vertex3f { position: { [-5.0, -5.0,  -5.0] } }); 
-    point.push(Vertex3f { position: { [-5.0,  5.0,  -5.0] } });
-    point.push(Vertex3f { position: { [ 5.0,  5.0,  -5.0] } });
-    point.push(Vertex3f { position: { [ 5.0, -5.0,  -5.0] } });
-    
-    point.push(Vertex3f { position: { [-5.0, -5.0,   5.0] } });
-    point.push(Vertex3f { position: { [-5.0,  5.0,   5.0] } });
-    point.push(Vertex3f { position: { [ 5.0,  5.0,   5.0] } });
-    point.push(Vertex3f { position: { [ 5.0, -5.0,   5.0] } });
-    
-    let mut image: Vec<Vertex2f> = convert_point_3d_to_2d(point);
+fn array_to_image(image: &mut Vec<[f64; 2]>) {
+    println!("Creating 2D projection");
     
     let mut image_lengthx: i64 = 0;
     let mut image_lengthy: i64 = 0;
     
     for i in image.iter_mut() {
-      i.position[0] *= 10.0;
-      i.position[1] *= 10.0;
+      i[0] *= 10.0;
+      i[1] *= 10.0;
       
-      if i.position[0] as i64 > image_lengthx {
-        image_lengthx = i.position[0] as i64;
+      if i[0] as i64 > image_lengthx {
+        image_lengthx = i[0] as i64;
       }
-      if i.position[1] as i64 > image_lengthy {
-        image_lengthy = i.position[1] as i64;
+      if i[1] as i64 > image_lengthy {
+        image_lengthy = i[1] as i64;
       }
     } 
     
@@ -112,7 +90,7 @@ fn main() {
     let sizex: u32 = image_lengthx as u32 * 2; 
     let sizey: u32 = image_lengthy as u32 * 2;
     
-    println!("Image Width: {} Height: {}", sizex, sizey);
+    println!("    image Width: {} Height: {}", sizex, sizey);
     
     let mut imgbuffer = image::ImageBuffer::new(sizex, sizey);
     
@@ -122,8 +100,8 @@ fn main() {
       
       for i in image.iter() {
 
-        if i.position[0] as i64 == crnt_px {
-         if i.position[1] as i64 == crnt_py {
+        if i[0] as i64 == crnt_px {
+         if i[1] as i64 == crnt_py {
             // Place white pixel
             *pixel = image::Luma([255]);
           }
@@ -134,4 +112,70 @@ fn main() {
     let ref mut fout = File::create(&Path::new("2DVisualisation.png")).unwrap();
     
     let _ = image::ImageLuma8(imgbuffer).save(fout, image::PNG);
+    
+    println!("Projection created!!");
 }
+
+fn read_3d_data(file: &str) -> Vec<[f64; 3]> {
+    let mut array_data = Vec::new();
+    
+    let mut rdr = csv::Reader::from_file(file).unwrap();
+    for record in rdr.decode() {
+        let (x, y, z): (f64, f64, f64) = record.unwrap();
+        array_data.push([x as f64, y as f64, z as f64]);
+        println!("{}, {}, {}", x, y, z);
+    }
+    
+    array_data
+}
+
+fn oblique_projection_from_3d(file: &str) {
+  let data = read_3d_data(file);
+  
+  let angle: f64 = 63.4;
+  let mut array_2d = Vec::new();
+  
+  for position in data {
+    let (x, y, z): (f64, f64, f64) = (position[0], position[1], position[2]);
+    array_2d.push([x + 0.5*z*angle.cos(), y + 0.5*z*angle.sin()])
+  }
+  
+  array_to_image(&mut array_2d);
+}
+
+fn main() {    
+    oblique_projection_from_3d("./data/cube.csv");
+}
+
+/*
+    let mut point = Vec::new();
+
+    point.push([-5.0, -5.0,  -5.0]); 
+    point.push([-5.0,  5.0,  -5.0]);
+    point.push([ 5.0,  5.0,  -5.0]);
+    point.push([ 5.0, -5.0,  -5.0]);
+    
+    point.push([-5.0, -5.0,   5.0]);
+    point.push([-5.0,  5.0,   5.0]);
+    point.push([ 5.0,  5.0,   5.0]);
+    point.push([ 5.0, -5.0,   5.0]);
+    
+    
+    convert_nth_dim_to_2_dim(point);
+
+fn convert_nth_dim_to_2_dim<Vec: AsRef<[Dimension]>, Dimension: AsRef<[V]>, V: Display>(md_array: Vec) {
+  // Each dimension x, y, z, 4th, 5th, 6th etc...
+ // println!("{}", md_array.len());
+  let md_array = md_array.as_ref();
+  print!("\t[");
+  for dimension in md_array {
+    let dimension = dimension.as_ref();
+
+    // Every variable in each dimension
+    for i in dimension {
+        print!("{}, ", i);
+    }
+    println!("; {}]", dimension.len());
+  }
+  println!("; {}]", md_array.len());
+}*/
